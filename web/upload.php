fc4ea90d6831f86dba389303106a6a1dc6ee48e4
John Wee <jobwer@gmail.com>
2017-02-08 16:35:41 +0200

    added variable body that mast get content from message

--- web/upload.php
+++ web/upload.php
@@ -4,6 +4,8 @@ require "../vendor/autoload.php";
 use Core\Http\ServerRequest;
 
 $serverRequest = ServerRequest::buildFromGlobals();
+$body = $serverRequest->getBody()->getContents();
 $parsedBody = $serverRequest->getParsedBody();
 print_r($parsedBody);
+print_r($body);
 
